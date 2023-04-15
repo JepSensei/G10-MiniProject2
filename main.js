fba979a7c95e3f0a8bee027ec767fd733134c05a
Charles Adrian Virtucio <101638556+charles-virtucio@users.noreply.github.com>
2023-04-15 23:30:06 +0800

    add function icon

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ $(".carousel").flipster({
     spacing: -0.3,
 
   });
+
+// Script for making icon as button
+document.getElementById('paper-plane-icon').addEventListener('click', function() {
+    // Add your desired action here, e.g. submit form, trigger AJAX request, etc.
+    alert('Paper airplane clicked!');
+});
