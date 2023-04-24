773dcbf80d390fcc8291e76e4ed64e304ba4181a
Charles Adrian Virtucio <101638556+charles-virtucio@users.noreply.github.com>
2023-04-24 22:45:21 +0800

    Update responses.js

--- responses.js
+++ responses.js
@@ -10,7 +10,7 @@ function getBotResponse(input) {
        return "Here's our menu: <br /><br /> Coffee Beans - Arabica Medium Roast - ₱265.00 <br /> Coffee Beans - Barako Medium Roast - ₱235.00 <br />  Coffee Beans - Benguet Blend - ₱235.00 <br /> Coffee Beans - Daily Ritual 500g - ₱562.50 <br />  Coffee Beans - Espresso Blend  - ₱265.00 <br /> Coffee Beans - Yirgacheffe Misty Valley  - ₱950.00  Coffee Beans - Espresso Blend  - ₱265.00 <br /> Coffee Beans - Excelsa Cordillera  - ₱285.00 <br /> Coffee Beans - Gesha Village Lot 85  - ₱665.00 <br /> Coffee Beans - Excelsa Cordillera  - ₱285.00 <br /> Coffee Beans - Gesha Village Lot 85  - ₱665.00 <br /> Coffee Beans - Got Milk? 500g  - ₱775.00 <br /> Coffee Beans - House Blend Dark Roast - ₱265.00 <br /> Coffee Beans - In Love with Flavors - ₱280.00 <br /> Coffee Beans - Kabuhayan Blend 250g - ₱285.00 <br /> Brewing Equipments - 1000pc Cup Coffee Filter - ₱549.00 <br /> Brewing Equipments - White Porcelain Creamer - ₱1,129.00 <br /> Brewing Equipments - Plastic Sugar Pourer - ₱129.00 <br /> Brewing Equipments - Rolled Edged Espresso Cup - ₱ 569.50";
     } else if (input == "about") {
         return "Hi there! <br /><br /> KapeTann Brewed Coffee is a coffee shop and retailer in Zambales, Philippines.";
-    } else if (input == "contact us") {
+    } else if (input == "contact") {
         return "Here's our contact information: <br /><br /> Email: abfiguerrez18@gmail.com <br /> Phone Number: 0917 134 1422 <br /> Messenger: @kapetanncoffee <br /> Address: Laoag, San Marcelino, Zambales ";
      } else if (input == "location") {
         return "Here's our address: <strong>San Marcelino, Zambales 2207 Olongapo, Philippines</strong>";
